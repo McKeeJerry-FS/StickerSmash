d953bdba4d164f514ae933424788f6d80e5e65d5
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-12-03 13:18:03 -0500

    created a components directory. created an ImageViewer component to be used in the application

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,14 @@
-import { Text, View, StyleSheet } from "react-native";
-import { Link } from 'expo-router';
+import { View, StyleSheet } from "react-native";
+import ImageViewer from '@/components/ImageViewer';
+
+const placeholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
-      <Text style={styles.text}>Home Screen</Text>
-      <Link href="/about" style={styles.button}> Go to About Screen</Link>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={placeholderImage} />
+      </View>
     </View>
   );
 }
@@ -26,5 +27,8 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textDecorationLine: 'underline',
     color: '#fff'
-  }
+  },
+  imageContainer: {
+    flex:1,
+  },
 })
