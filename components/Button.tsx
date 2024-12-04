cf2023f4bddfe000eb4b8239ca3650c87ddf0f81
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-12-04 10:09:42 -0500

    Added an emoji picker to the application

--- components/Button.tsx
+++ components/Button.tsx
@@ -12,20 +12,20 @@ export default function Button({ label, theme, onPress }: Props) {
         return(
             <View
         style={[
-          styles.buttonContainer,
-          { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
+        styles.buttonContainer,
+        { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
         ]}>
-        <Pressable
-          style={[styles.button, { backgroundColor: '#fff' }]} onPress={onPress}>
-          <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
-          <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
-        </Pressable>
-      </View>
+    <Pressable
+        style={[styles.button, { backgroundColor: '#fff' }]} onPress={onPress}>
+        <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
+        <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
+    </Pressable>
+    </View>
         );
     }
         return(
             <View style={styles.buttonContainer}>
-            <Pressable style={styles.button} onPress={() => alert('You pressed the button.')}>
+            <Pressable style={styles.button} onPress={onPress}>
                 <Text style={styles.buttonLabel}>{label}</Text>
             </Pressable>
         </View>
@@ -52,7 +52,7 @@ const styles = StyleSheet.create({
     },
     buttonIcon: {
         paddingRight: 8,
-      },
+    },
     buttonLabel: {
         color: '#fff',
         fontSize: 16,
